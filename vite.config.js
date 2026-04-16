1c6418f1e89f3b3d7cd1ce1d46841ea074da7875
denlehov28-creator <denlehov28@gmail.com>
2026-04-16 11:51:02 +0300

    disable pwa for first deploy

--- vite.config.js
+++ vite.config.js
@@ -1,45 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    // Configure PWA support
-    VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: [],
-      manifest: {
-        name: 'TARAN Boxing Club',
-        short_name: 'TARAN',
-        description: 'Супер‑приложение боксерского клуба TARAN',
-        theme_color: '#101010',
-        background_color: '#101010',
-        display: 'standalone',
-        scope: '/',
-        start_url: '/',
-        icons: [
-          {
-            src: 'icons/icon-192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: 'icons/icon-512x512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          },
-          {
-            src: 'icons/icon-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable'
-          }
-        ]
-      }
-    })
-  ],
+  plugins: [react()],
   server: {
     port: 3000
   }
